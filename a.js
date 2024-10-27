318f76c332836768967ac2fca164ea18dcdc9776
ranutomar18 <ranutomar0018@gmail.com>
2024-10-27 23:31:40 +0530

    Delete a.js

--- a.js
+++
@@ -1,9 +0,0 @@
-const options = {
-    method: 'POST',
-    headers: {accept: 'application/json', 'content-type': 'application/json'}
-  };
-  
-  fetch('https://api.qase.io/v1/custom_field', options)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
